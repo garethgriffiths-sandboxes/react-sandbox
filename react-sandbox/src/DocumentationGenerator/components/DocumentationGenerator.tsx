c10e317db8a836a96b2cdd719a735efcb8897acd
“MrGarethTG” <43384409+MrGarethTG@users.noreply.github.com>
2021-11-28 00:10:19 +0000

    updates

--- react-sandbox/src/DocumentationGenerator/components/DocumentationGenerator.tsx
+++ react-sandbox/src/DocumentationGenerator/components/DocumentationGenerator.tsx
@@ -5,8 +5,8 @@ import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin';
 
 const DocumentationGenerator = () => {
     const ref = useRef<any>();
+    const iframe = useRef<any>();
     const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -24,6 +24,8 @@ const DocumentationGenerator = () => {
             return;
         }
 
+        iframe.current.srcdoc = html;
+
         const result = await ref.current.build(
             {
                 entryPoints: ['index.js'],
@@ -36,12 +38,27 @@ const DocumentationGenerator = () => {
                 },
             });
 
-        setCode(result.outputFiles[0].text);
-        try {
-        }
-        catch (err) {
-        }
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
     }
+
+    const html = `<html>
+    <head>
+    <body>
+    <div id="root"></div>
+    <script>
+    window.addEventListener('message', (event) => {
+        try {
+        eval(event.data);
+        } catch (err) {
+            const root = document.querySelector('#root');
+            root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+            console.error(err);
+        }
+    }, false);
+    </script>
+    </body>
+    </head>
+    </html>`;
 
     return <div>
         <textarea value={input} onChange={(e) => setInput(e.target.value)}>
@@ -49,7 +66,7 @@ const DocumentationGenerator = () => {
         <div>
             <button onClick={onClick}>Submit</button>
         </div>
-        <pre>{code}</pre>
+        <iframe title="code-html" ref={iframe} sandbox="allow-scripts" srcDoc={html}></iframe>
     </div>;
 };
 
